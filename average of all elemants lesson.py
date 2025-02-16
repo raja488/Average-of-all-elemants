d69cc33ed4659a4395a534badcf4a99c85a7b1de
umar <umarqadoos93@gmail.com>
2025-02-16 15:05:39 +0000

    finished

--- average of all elemants lesson.py
+++ average of all elemants lesson.py
@@ -2,5 +2,5 @@ numbers=[2,3,4,5,6,4,56,64,45,345]
 # formula to calculate average = add all the numbers up/by how many numbers there are 
 average=sum(numbers)/len(numbers)
 print(average)
-names=["UMAR","RAJA","SYED","ISA","ENGJULL","MARK","LEO","ASAD","ALI","HARRY"]
-if (names start with A):
+names=["UMAR", "RAJA", "SYED", "ISA", "ENGJULL", "MARK", "LEO", "ASAD", "ALI", "HARRY"]
+print(" ".join(sorted(names)))
