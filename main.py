42ccdeb78b30df3d9dc99381a7f198504ab304bb
umar <umarqadoos93@gmail.com>
2025-02-16 18:37:25 +0000

    average elemants lesson

---
+++ main.py
@@ -0,0 +1,6 @@
+numbers=[2,3,4,5,6,4,56,64,45,345]
+# formula to calculate average = add all the numbers up/by how many numbers there are 
+average=sum(numbers)/len(numbers)
+print(average)
+names=["UMAR","RAJA","SYED","ISA","ENGJULL","MARK","LEO","ASAD","ALI","HARRY"]
+if (names start with A):
